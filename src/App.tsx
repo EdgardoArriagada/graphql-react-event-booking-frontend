4f5ed4ef765a3411830172ec68eb75ab1aff1eec
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-04-16 17:23:28 -0400

    update App.tsx to show these pages in a BrowserRouter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
+import AuthPage from './pages/Auth';
+import EventsPage from './pages/Events';
+import BookingsPage from './pages/Bookings';
 
-class App extends Component {
-    render() {
-        return <div className="App" />;
-    }
-}
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Redirect from="/" to="/auth" exact />
+                <Route path="/auth" component={AuthPage} />
+                <Route path="/events" component={EventsPage} />
+                <Route path="/bookings" component={BookingsPage} />
+            </Switch>
+        </BrowserRouter>
+    );
+};
 
 export default App;

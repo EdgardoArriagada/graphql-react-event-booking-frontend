667dd4c930f23dda414d506945e5205869c411b9
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-04-17 15:52:29 -0400

    add a sandwich button drawer

---
+++ src/components/Navigation/DrawerToggleButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './drawerToggleButton.scss';
+
+const DrawerToggleButton = () => (
+    <button className="drawer-toggle-button">
+        <div className="drawer-toggle-button__line" />
+        <div className="drawer-toggle-button__line" />
+        <div className="drawer-toggle-button__line" />
+    </button>
+);
+
+export default DrawerToggleButton;

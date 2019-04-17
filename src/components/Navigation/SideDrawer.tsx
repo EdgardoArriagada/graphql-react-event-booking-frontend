3e980421fa45ae39c230d33b4bf3a87967d3bf77
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-04-17 15:52:51 -0400

    add a sidedrawer component

---
+++ src/components/Navigation/SideDrawer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavigationItems from './NavigationItems';
+
+import './sideDrawer.scss';
+
+const SideDrawer = () => (
+    <span className="side-drawer">
+        <NavigationItems />
+    </span>
+);
+
+export default SideDrawer;

2b6488163a63fbd9fcf366d5d76c2f46d3512c95
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-04-21 21:47:28 -0400

    add a new menu icon for when user is logged in

--- src/components/Navigation/MainNavigation.tsx
+++ src/components/Navigation/MainNavigation.tsx
@@ -1,21 +1,51 @@
-import React from 'react';
-
-import './mainNavigation.scss';
+import React, { useState } from 'react';
 import DrawerToggleButton from './DrawerToggleButton';
 import NavigationItems from './NavigationItems';
+import MuIconButton from '@material-ui/core/IconButton';
+import MuAccountCircle from '@material-ui/icons/AccountCircle';
+import MuMenu from '@material-ui/core/Menu';
+import MuMenuItem from '@material-ui/core/MenuItem';
 
-const MainNavigation = () => (
-    <header className="main-navigation">
-        <span className="main-navigation__logo">
-            <span className="main-navigation__button">
-                <DrawerToggleButton />
+import './mainNavigation.scss';
+import { useStateValue } from '../../Store/Store';
+
+const MainNavigation = () => {
+    const { AuthState } = useStateValue();
+    const userLoggedIn = Boolean(AuthState.token);
+    const [anchorEl, setAnchorEl] = useState();
+    const isMenuOpen = Boolean(anchorEl);
+    function handleProfileMenuOpen(event: React.MouseEvent<HTMLElement>) {
+        setAnchorEl(event.currentTarget);
+    }
+
+    const renderMenu = (
+        <MuMenu anchorEl={anchorEl} open={isMenuOpen} onClose={_ => setAnchorEl(null)}>
+            <MuMenuItem onClick={void 0}>Profile</MuMenuItem>
+            <MuMenuItem onClick={void 0}>Logout</MuMenuItem>
+        </MuMenu>
+    );
+
+    return (
+        <header className="main-navigation">
+            <span className="main-navigation__logo">
+                <span className="main-navigation__button">
+                    <DrawerToggleButton />
+                </span>
+                <h1>EasyEvent</h1>
             </span>
-            <h1>EasyEvent</h1>
-        </span>
-        <span className="main-navigation__items">
-            <NavigationItems />
-        </span>
-    </header>
-);
+            <span className="main-navigation__items">
+                <span className="main-navigation__links">
+                    <NavigationItems />
+                </span>
+                {userLoggedIn && (
+                    <MuIconButton onClick={handleProfileMenuOpen}>
+                        <MuAccountCircle className="account-circle" />
+                    </MuIconButton>
+                )}
+            </span>
+            {renderMenu}
+        </header>
+    );
+};
 
 export default MainNavigation;

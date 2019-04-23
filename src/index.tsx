441b86c2aff89e6bbeb57e10a9f69466649d186d
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-04-22 20:08:43 -0400

    add muiTheme to the app with basic theme

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import { StateProvider } from './Store/Store';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#800080',
+        },
+    },
+});
+
+export const styles = {
+    primaryButton: {
+        margin: '0.25rem 0.1rem',
+    },
+};
 
 ReactDOM.render(
-    <StateProvider>
-        <App />
-    </StateProvider>,
+    <MuiThemeProvider theme={theme}>
+        <StateProvider>
+            <App />
+        </StateProvider>
+    </MuiThemeProvider>,
     document.getElementById('root'),
 );

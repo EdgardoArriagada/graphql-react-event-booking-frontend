02e0bde5ab7080a5d613bf20a96884858ad14704
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-04-24 17:45:54 -0400

    create AppModalContent component to wrap app modal content with default styles

---
+++ src/components/sharedComponents/AppModalContent.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Theme, withStyles, WithStyles } from '@material-ui/core';
+import { appStyles, IStyles } from '../../shared/styles/styles';
+
+const style = (theme: Theme): IStyles => ({
+    paper: {
+        position: 'absolute',
+        width: theme.spacing.unit * 50,
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing.unit * 4,
+        outline: 'none',
+    },
+});
+
+type Props = {
+    children: JSX.Element;
+};
+
+type PropsWithStyles = Props & WithStyles<'paper'>;
+
+const AppModalContent: React.SFC<PropsWithStyles> = ({ classes, ...props }: PropsWithStyles) => {
+    return (
+        <div className={classes.paper} style={appStyles.modalCentered}>
+            {props.children}
+        </div>
+    );
+};
+
+export default withStyles(style as any)(AppModalContent);

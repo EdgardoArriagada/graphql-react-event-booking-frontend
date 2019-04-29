dc4569ce7b702b53787e4897d5303516ea8cef22
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-04-29 19:00:26 -0400

    add some button and styling to the EventItem component

--- src/components/Events/EventsList/EventItem.tsx
+++ src/components/Events/EventsList/EventItem.tsx
@@ -1,33 +1,83 @@
 import React from 'react';
-import { Card, Typography, Theme, withStyles, WithStyles, CardContent, CardHeader } from '@material-ui/core';
+import {
+    Card,
+    Typography,
+    Theme,
+    withStyles,
+    WithStyles,
+    CardContent,
+    CardHeader,
+    IconButton,
+    CardActions,
+    Button,
+} from '@material-ui/core';
 import { IStyles, appClasses } from '../../../shared/styles/styles';
-import EventList from './EventList';
+import { Create } from '@material-ui/icons';
+import classNames from 'classnames';
 
 const style = (theme: Theme): IStyles => ({
     card: { ...appClasses.card },
-    listItem: {
-        width: '100%',
+    cardContent: {
+        width: '90%',
         display: 'flex',
-        flexDirection: 'column',
-        alignContent: 'flex-start',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        flexWrap: 'wrap',
+        margin: '-1rem 0', // gutter hack :-(
+    },
+    cardContentItem: {
+        margin: '1rem 0', // gutter hack :-(
+        flex: '1 0 200px',
+    },
+    cardHeader: {
+        width: '90%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    cardDescription: {
+        marginRight: '1rem',
+    },
+    cardActions: {
+        width: '95%',
+        display: 'flex',
+        justifyContent: 'flex-end',
     },
 });
 
-type PropsWithStyles = Props & WithStyles<'card' | 'listItem'>;
+type PropsWithStyles = Props &
+    WithStyles<'card' | 'cardContent' | 'cardHeader' | 'cardContentItem' | 'cardDescription' | 'cardActions'>;
 
 const EventItem: React.SFC<PropsWithStyles> = ({ classes, ...props }: PropsWithStyles) => {
     const { event } = props;
     return (
         <Card className={classes.card} key={event._id}>
-            <CardContent className={classes.listItem}>
-                <CardHeader title={event.title} />
-                <CardContent>
-                    <Typography variant="subtitle1" gutterBottom>
+            <CardHeader
+                className={classes.cardHeader}
+                title={event.title}
+                action={
+                    <IconButton aria-label="Edit">
+                        <Create fontSize="small" />
+                    </IconButton>
+                }
+            />
+            <CardContent className={classes.cardContent}>
+                <span className={classNames(classes.cardContentItem, classes.cardDescription)}>
+                    <Typography variant="body1" gutterBottom>
                         {event.description}
                     </Typography>
-                    <Typography>{event.price} USD </Typography>
-                </CardContent>
+                    <Typography variant="h6">{event.price} USD </Typography>
+                </span>
+                <span className={classes.cardContentItem}>
+                    <Typography variant="caption">You are the owner of this event</Typography>
+                </span>
             </CardContent>
+            <CardActions className={classes.cardActions}>
+                <Button variant="text">Details</Button>
+                <Button variant="text" color="primary">
+                    Book this
+                </Button>
+            </CardActions>
         </Card>
     );
 };

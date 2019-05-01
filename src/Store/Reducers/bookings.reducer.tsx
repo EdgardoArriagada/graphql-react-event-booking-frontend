073a9e98001c7ac0bd945f2d6e28dd131a006b6f
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-05-01 00:54:41 -0400

    add actions and reducers necessary to cancel a booking

--- src/Store/Reducers/bookings.reducer.tsx
+++ src/Store/Reducers/bookings.reducer.tsx
@@ -1,6 +1,6 @@
 import { Dispatch } from 'react';
 import { Status } from './utils/types';
-import { IEvent } from '../../shared/models/event.model';
+import { IBooking } from '../../shared/models/booking.model';
 
 export interface BookingsStore {
     BookingsState: BookingsState;
@@ -9,7 +9,7 @@ export interface BookingsStore {
 
 export interface BookingsState {
     status: Status;
-    bookings: Array<IEvent>;
+    bookings: Array<IBooking>;
 }
 
 export const initialBookingsState: BookingsState = { bookings: [], status: 'PRISTINE' };
@@ -17,10 +17,15 @@ export const initialBookingsState: BookingsState = { bookings: [], status: 'PRIS
 export const BookingsReducer = (state: BookingsState, action: BookingsActions): BookingsState => {
     switch (action.type) {
         case 'FETCH_BOOKINGS_PENDING':
+        case 'CANCEL_BOOKINGS_PENDING':
             return { bookings: [...state.bookings], status: 'PENDING' };
         case 'FETCH_BOOKINGS_FULFILLED':
             return { bookings: [...action.bookings], status: 'FULFILLED' };
+        case 'CANCEL_BOOKINGS_FULFILLED':
+            const updatedBookings = state.bookings.filter(booking => action.bookingId !== booking._id);
+            return { bookings: updatedBookings, status: 'FULFILLED' };
         case 'FETCH_BOOKINGS_REJECTED':
+        case 'CANCEL_BOOKINGS_REJECTED':
             return { bookings: [...state.bookings], status: 'REJECTED' };
         default:
             return state || initialBookingsState;
@@ -28,16 +33,20 @@ export const BookingsReducer = (state: BookingsState, action: BookingsActions): 
 };
 
 // Actions
-interface FetchBookingsPending {
-    type: 'FETCH_BOOKINGS_PENDING';
+interface BookingsPending {
+    type: 'FETCH_BOOKINGS_PENDING' | 'CANCEL_BOOKINGS_PENDING';
 }
 
-interface FetchBookingsRejected {
-    type: 'FETCH_BOOKINGS_REJECTED';
+interface bookingsRejected {
+    type: 'FETCH_BOOKINGS_REJECTED' | 'CANCEL_BOOKINGS_REJECTED';
 }
-interface FetchBookingsFulfilled {
+interface fetchBookingsFulfilled {
     type: 'FETCH_BOOKINGS_FULFILLED';
     bookings: BookingsState['bookings'];
 }
+interface cancelBookingsFulfilled {
+    type: 'CANCEL_BOOKINGS_FULFILLED';
+    bookingId: IBooking['_id'];
+}
 
-type BookingsActions = FetchBookingsPending | FetchBookingsRejected | FetchBookingsFulfilled;
+type BookingsActions = BookingsPending | bookingsRejected | fetchBookingsFulfilled | cancelBookingsFulfilled;

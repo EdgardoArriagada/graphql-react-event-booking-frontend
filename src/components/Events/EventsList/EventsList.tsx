53d2702c9cd7543e52080b48528b655aaae3b157
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-05-01 00:55:49 -0400

    allow user to delete their own bookings

--- src/components/Events/EventsList/EventsList.tsx
+++ src/components/Events/EventsList/EventsList.tsx
@@ -4,19 +4,15 @@ import { withStyles, WithStyles, Theme } from '@material-ui/core';
 import { useStateValue } from '../../../Store/Store';
 import { IStyles } from '../../../shared/models/styles.model';
 
-const style = (theme: Theme): IStyles => ({
-    list: {
-        listStyle: 'none',
-    },
-});
+const style = (theme: Theme): IStyles => ({});
 
-type PropsWithStyles = Props & WithStyles<'list'>;
+type PropsWithStyles = Props & WithStyles<''>;
 
 const EventsList: React.SFC<PropsWithStyles> = ({ classes }: PropsWithStyles) => {
     const { EventsState } = useStateValue();
     const EventsList = EventsState.events.map((event: any) => <EventItem event={event} key={event._id} />);
 
-    return <section className={classes.list}>{EventsList}</section>;
+    return <section>{EventsList}</section>;
 };
 
 type Props = {};

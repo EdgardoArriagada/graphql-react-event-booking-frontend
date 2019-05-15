0fa00fdb2297678d92047319be2fc1b70363a63d
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-05-15 13:22:21 -0400

    Updates

---
+++ precache-manifest.d68546bc0a66cb779cdc11168fd1bfe5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2e5e7c2a317ddc080351",
+    "url": "/static/js/main.797605c0.chunk.js"
+  },
+  {
+    "revision": "b6b2b3a2b91e76272a52",
+    "url": "/static/js/2.7d03edc2.chunk.js"
+  },
+  {
+    "revision": "2e5e7c2a317ddc080351",
+    "url": "/static/css/main.af2318ee.chunk.css"
+  },
+  {
+    "revision": "6348f4765e9eb7678705844a34b9ffac",
+    "url": "/index.html"
+  }
+];

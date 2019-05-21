284ffa1d0b5ea44ce65d2e5d64efba19028dbd0f
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-05-21 16:24:41 -0400

    Updates

--- precache-manifest.462c9025e6929638b07b576f85f68a25.js
+++ precache-manifest.462c9025e6929638b07b576f85f68a25.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/graphql-react-event-booking-frontend/static/js/runtime~main.3a25ec3f.js"
   },
   {
-    "revision": "b866dc559815325ffe67",
-    "url": "/graphql-react-event-booking-frontend/static/js/main.2c29d5e1.chunk.js"
+    "revision": "1455f802154aefa9aed5",
+    "url": "/graphql-react-event-booking-frontend/static/js/main.94c05194.chunk.js"
   },
   {
-    "revision": "b6b2b3a2b91e76272a52",
-    "url": "/graphql-react-event-booking-frontend/static/js/2.7d03edc2.chunk.js"
+    "revision": "2f2a4fc2573985aeca20",
+    "url": "/graphql-react-event-booking-frontend/static/js/2.e6b87b96.chunk.js"
   },
   {
-    "revision": "b866dc559815325ffe67",
+    "revision": "1455f802154aefa9aed5",
     "url": "/graphql-react-event-booking-frontend/static/css/main.af2318ee.chunk.css"
   },
   {
-    "revision": "33eeb60934ed466df4385b4c52f2b8e8",
+    "revision": "6667c9afa792786b7520f3dd79053dc2",
     "url": "/graphql-react-event-booking-frontend/index.html"
   }
 ];

09598beddfc9b6ffca235249392bbd3f74d27980
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-05-21 21:53:48 -0400

    add a message card for when there are no events

--- src/components/Events/EventsList/EventsList.tsx
+++ src/components/Events/EventsList/EventsList.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import EventItem from './EventItem';
-import { withStyles, WithStyles, Theme } from '@material-ui/core';
+import { withStyles, WithStyles, Theme, Card, Typography } from '@material-ui/core';
 import { useStateValue } from '../../../Store/Store';
 import { IStyles } from '../../../shared/models/styles.model';
+import { appClasses } from '../../../shared/styles/styles';
 
-const style = (theme: Theme): IStyles => ({});
+const style = (theme: Theme): IStyles => ({
+    card: {
+        padding: '3rem',
+        ...appClasses.card,
+    },
+});
 
-type PropsWithStyles = Props & WithStyles<''>;
+type PropsWithStyles = Props & WithStyles<'card'>;
 
 const EventsList: React.SFC<PropsWithStyles> = ({ classes }: PropsWithStyles) => {
     const { EventsState } = useStateValue();
-    const EventsList = EventsState.events.map((event: any) => <EventItem event={event} key={event._id} />);
+    const EventsList =
+        EventsState.events.length > 0 ? (
+            EventsState.events.map((event: any) => <EventItem event={event} key={event._id} />)
+        ) : (
+            <Card className={classes.card}>
+                <Typography variant="h5">There are no events yet</Typography>
+            </Card>
+        );
 
     return <section>{EventsList}</section>;
 };
